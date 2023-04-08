14720003cf3a0800565bfa9a35c2163b727352ec
jamil gafur <jamilggafur@gmail.com>
2023-04-08 16:31:49 -0500

    updating setup

--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
 from setuptools import setup, find_packages
 
 VERSION = '0.0.1'
-DESCRIPTION = 'A things package'
-LONG_DESCRIPTION = 'A package that  does things'
+DESCRIPTION = 'This package is used to help with the interpretation and adversarial generation of pytorch models.'
+LONG_DESCRIPTION = 'The "Adversarial Observation" framework is introduced to address concerns about the fairness and social impact of neural network models by allowing users to attack the network for adversarial resistance. This framework increases the explainability and transparency of the network using an adversarial swarm optimizer, making it more interpretable for stakeholders. The framework has the potential to improve the social impact of neural network models and enhance their effectiveness and efficiency by providing a user-friendly approach to adversarial testing and explainability.'
+
+authors = {
+    'author': 'Author Name',
+    'author_email': '
+}
 
 setup(
     name="Adversarial_Observation",
     version=VERSION,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    author="<Your Name>",
+    author=["Jamil Gafur", "Study Duck", "Suely Oliveira", "Olivia Lang", "Justin Cha", "William Lai"]
+    author_email=["Jamil-gafur@uiowa.edu"]
     author_email="<your email>",
     license='MIT',
     packages=find_packages(),
-    install_requires=[],
-    keywords='conversion',
+    install_requires=[torch, numpy, tqdm, pandas, matplotlib, sklearn, os, torchvision, pickle ],
+    keywords=['Interpretability', 'Adversarial', 'Explainability', 'Swarm', 'PSO', 'Pytorch', 'Neural Networks', 'Machine Learning', 'Deep Learning'],
     classifiers= [
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
